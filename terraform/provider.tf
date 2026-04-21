f39b18c97f6add41d205d7ddd92eb3b20f80e007
Dan Lima <dantiago@hotmail.com>
2026-04-21 14:36:11 +0200

    Fix pipeline to use remote Terraform backend

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = ">= 1.5.0"
 
+  backend "azurerm" {
+    resource_group_name  = "rg-tf-state"
+    storage_account_name = "tfstate69837"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"

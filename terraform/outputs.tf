ee977526e2521255a6760df1949b9a23468990d8
Dan Lima <dantiago@hotmail.com>
2026-04-21 14:51:22 +0200

    Finalize remote backend and Terraform outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,11 @@
 output "public_ip" {
-  description = "Public IP of the Terraform VM"
-  value       = azurerm_public_ip.pip.ip_address
+  value = azurerm_public_ip.pip.ip_address
 }
 
 output "vm_name" {
-  description = "Terraform VM name"
-  value       = azurerm_linux_virtual_machine.vm.name
+  value = azurerm_linux_virtual_machine.vm.name
 }
 
 output "resource_group_name" {
-  description = "Terraform resource group name"
-  value       = azurerm_resource_group.rg.name
+  value = azurerm_resource_group.rg.name
 }
